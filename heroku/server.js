f3939d96e6ad0b5df70891b5c61f380c85cc1732
lei <82316160+leilapatricia@users.noreply.github.com>
2022-11-17 03:03:38 +0800

    Update server.js

--- heroku/server.js
+++ heroku/server.js
@@ -9,11 +9,9 @@ corsProxy.createServer({
     originWhitelist: [
       'http://localhost:3000',
       'http://localhost:5000',
-      'https://insta-profile-pic.web.app',
-      'https://insta-profile-pic.firebaseapp.com',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com/',
-      'https://portfolio-ravenous.herokuapp.com'
+      'http://localhost:5000',
+      'https://listed-e50b8.firebaseapp.com/',
+      'https://listed-e50b8.web.app/',
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
